4db90d444411a3c823745df4c50589cb6cf37960
Anthony Picciano <anthony.picciano@gmail.com>
2018-01-12 10:50:45 -0700

    Initial Commit

---
+++ SendGrid-Swift/SendGrid_Swift.h
@@ -0,0 +1,19 @@
+//
+//  SendGrid_Swift.h
+//  SendGrid-Swift
+//
+//  Created by Anthony Picciano on 1/12/18.
+//  Copyright © 2018 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SendGrid_Swift.
+FOUNDATION_EXPORT double SendGrid_SwiftVersionNumber;
+
+//! Project version string for SendGrid_Swift.
+FOUNDATION_EXPORT const unsigned char SendGrid_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SendGrid_Swift/PublicHeader.h>
+
+
